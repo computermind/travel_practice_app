f2e81e1f95010f45416f3b07cccfbb9452659063
Thomas Guwor <guworthomas@gmail.com>
2016-12-18 21:19:06 +0000

    query string for color, size and added pictures

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @countries = ['Oman', 'Croatia', 'Peru', 'Liberia']
+    @liberia = ['beach_liberia.jpg', 'Executive_Mansion.jpg', 'tour_liberia.jpg', 'waterfall-liberia.jpg']
   end
 
   def about
+    @color = params[:color]
+    @size = params[:size].to_i
   end
 end
