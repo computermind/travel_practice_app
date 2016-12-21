eaa6a88203f073015c69eccc03220d9dec27c237
Thomas Guwor <guworthomas@gmail.com>
2016-12-21 01:14:22 +0000

    Add avatar and views to destinations

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @countries = ['Oman', 'United States', 'Croatia', 'Peru', 'Liberia']
     @liberia_tourism = ['beach_liberia.jpg', 'Executive_Mansion.jpg', 'tour_liberia.jpg', 'waterfall-liberia.jpg']
-    @united_states = {"capital city" => "Washington, DC", 
-                     "favorite city" => "Asheville, NC", 
-                     "favorite state" => "Oregon", 
-                     "flag colors" => ["red", "white", "blue"]}
   end
 
   def about
