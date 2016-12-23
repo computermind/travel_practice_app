211b6837f20452697c54b43c5354e5615327da8e
Thomas Guwor <guworthomas@gmail.com>
2016-12-23 07:20:17 +0000

    Add gem figaro and values to destination: lgn, lat and address

---
+++ db/migrate/20161223071714_add_values_to_destination.rb
@@ -0,0 +1,7 @@
+class AddValuesToDestination < ActiveRecord::Migration
+  def change
+    add_column :destinations, :longitude, :float
+    add_column :destinations, :latitude, :float
+    add_column :destinations, :address, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220235635) do
+ActiveRecord::Schema.define(version: 20161223071714) do
 
   create_table "destinations", force: :cascade do |t|
     t.string   "city"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20161220235635) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.float    "longitude"
+    t.float    "latitude"
+    t.string   "address"
   end
 
 end
